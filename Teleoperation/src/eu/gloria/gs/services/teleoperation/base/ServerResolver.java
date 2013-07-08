c520e893f56019d8885e18e72a1952be5d7b962e
Fernando Serena <fserena@ciclope.info>
2013-07-08 14:11:40 +0200

    They are no longer needed.

--- Teleoperation/src/eu/gloria/gs/services/teleoperation/base/ServerResolver.java
+++
@@ -1,8 +0,0 @@
-package eu.gloria.gs.services.teleoperation.base;
-
-public interface ServerResolver {
-
-	public String resolve(String server) throws TeleoperationException;
-	public ServerHandler getHandler(String server);
-	
-}
