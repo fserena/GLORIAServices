b52faee8567d89e444ac5bc2b9d227bf9e598155
fserena <fserena@ciclope.info>
2013-07-11 16:36:46 +0200

    First version of the FilterWheel interface for GLORIA

---
+++ GLORIAInterfaces/src/eu/gloria/gs/services/teleoperation/fw/FilterWheelTeleoperationException.java
@@ -0,0 +1,18 @@
+package eu.gloria.gs.services.teleoperation.fw;
+
+import eu.gloria.gs.services.teleoperation.rts.RTSTeleoperationException;
+
+public class FilterWheelTeleoperationException extends RTSTeleoperationException {
+
+	/**
+	 * 
+	 */
+	public FilterWheelTeleoperationException(String message)
+	{
+		super(message);
+	}
+	
+	
+	private static final long serialVersionUID = 5848333110207976076L;
+
+}
