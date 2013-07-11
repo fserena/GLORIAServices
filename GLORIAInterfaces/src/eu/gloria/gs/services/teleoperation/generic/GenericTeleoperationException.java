2e2020c373148efba6960fbdd9bd60b55e08d84a
fserena <fserena@ciclope.info>
2013-07-11 14:55:02 +0200

    GenericTeloperation included (interactive mode ops)

---
+++ GLORIAInterfaces/src/eu/gloria/gs/services/teleoperation/generic/GenericTeleoperationException.java
@@ -0,0 +1,18 @@
+package eu.gloria.gs.services.teleoperation.generic;
+
+import eu.gloria.gs.services.teleoperation.rts.RTSTeleoperationException;
+
+public class GenericTeleoperationException extends RTSTeleoperationException {
+
+	/**
+	 * 
+	 */
+	public GenericTeleoperationException(String message)
+	{
+		super(message);
+	}
+	
+	
+	private static final long serialVersionUID = 5848333110207976076L;
+
+}
