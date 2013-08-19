ce8322efe934336508bdc20feb3c64cc816ea1d0
kudh <fserena@ciclope.info>
2013-08-19 10:33:39 +0200

    New exception for image database errors added

---
+++ GLORIAInterfaces/src/eu/gloria/gs/services/repository/image/data/ImageDatabaseException.java
@@ -0,0 +1,15 @@
+package eu.gloria.gs.services.repository.image.data;
+
+
+public class ImageDatabaseException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ImageDatabaseException(String message) {
+		super(message);
+	}
+	
+}
