afe8dde4390e729e29be8e00217b79094559dce9
Fernando Serena <fserena@ciclope.info>
2013-10-15 13:30:03 +0200

    Some classes moved

--- RTSClient/src/eu/gloria/gs/services/teleoperation/base/ServerResolver.java
+++ RTSClient/src/eu/gloria/gs/services/teleoperation/base/ServerResolver.java
@@ -1,5 +1,7 @@
 package eu.gloria.gs.services.teleoperation.base;
 
+import eu.gloria.gs.services.repository.rt.data.ServerKeyData;
+
 public interface ServerResolver {
 
 	public ServerKeyData resolve(String server) throws TeleoperationException;
