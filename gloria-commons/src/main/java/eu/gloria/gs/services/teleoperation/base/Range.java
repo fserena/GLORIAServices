ea952083dc9a55071117ccd7e02824d5daaf4d50
fserena <fserena@ciclope.info>
2014-05-11 22:49:53 +0200

    Range POJO

---
+++ gloria-commons/src/main/java/eu/gloria/gs/services/teleoperation/base/Range.java
@@ -0,0 +1,24 @@
+package eu.gloria.gs.services.teleoperation.base;
+
+public class Range {
+
+	private double min;
+	private double max;
+
+	public double getMin() {
+		return min;
+	}
+
+	public void setMin(double min) {
+		this.min = min;
+	}
+
+	public double getMax() {
+		return max;
+	}
+
+	public void setMax(double max) {
+		this.max = max;
+	}
+
+}
